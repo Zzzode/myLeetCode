612403959ae3a6ebde277fc0d6c7798562ba398c
zzzode <antifragile_zju@126.com>
2020-10-03 00:50:48 +0800

    add sol

---
+++ 1. 两数之和.cpp
@@ -0,0 +1,14 @@
+#include "Header.h"
+
+class Solution {
+ public:
+  vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> hash;
+    for (int i = 0; i < nums.size(); i++) {
+      if (hash.count(target - nums[i])) return {hash[target - nums[i]], i};
+      hash[nums[i]] = i;
+    }
+
+    return {};
+  }
+};
