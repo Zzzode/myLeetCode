7b3e3cab01978429fcd4c4d7736e897a9d4a672a
zzzode <antifragile_zju@126.com>
2020-10-13 16:13:46 +0800

    add solution

---
+++ 24.两两交换链表中的节点.cpp
@@ -0,0 +1,87 @@
+/*
+ * @lc app=leetcode.cn id=24 lang=cpp
+ *
+ * [24] 两两交换链表中的节点
+ *
+ * https://leetcode-cn.com/problems/swap-nodes-in-pairs/description/
+ *
+ * algorithms
+ * Medium (66.77%)
+ * Likes:    695
+ * Dislikes: 0
+ * Total Accepted:    173.2K
+ * Total Submissions: 255.9K
+ * Testcase Example:  '[1,2,3,4]'
+ *
+ * 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+ *
+ * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：head = [1,2,3,4]
+ * 输出：[2,1,4,3]
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：head = []
+ * 输出：[]
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：head = [1]
+ * 输出：[1]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 链表中节点的数目在范围 [0, 100] 内
+ * 0
+ *
+ *
+ */
+#include "Header.h"
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+ public:
+  ListNode* swapPairs(ListNode* head) {
+    if (!head) return head;
+    if (!head->next) return head;
+    ListNode *n = head->next, *p = head, *pre = nullptr;
+    head = n;
+    while (n) {
+      if (pre) pre->next = n;
+      p->next = n->next;
+      n->next = p;
+      pre = p;
+      if (p->next) {
+        p = p->next;
+        n = p->next;
+      } else
+        break;
+    }
+
+    return head;
+  }
+};
+// @lc code=end
