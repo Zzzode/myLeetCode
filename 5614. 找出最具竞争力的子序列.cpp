755563269924494a272a03d3ab3bfa9b7bd51a0d
Zzzode <antifragile_zju@126.com>
2020-11-29 21:33:01 +0800

    add sols

---
+++ 5614. 找出最具竞争力的子序列.cpp
@@ -0,0 +1,18 @@
+#include "Header.h"
+
+using pii = pair<int, int>;
+class Solution {
+ public:
+  vector<int> mostCompetitive(vector<int>& nums, int k) {
+    int n = nums.size();
+    map<int, set<int>> ma;
+    vector<int> ans;
+    for (int i = 0; i < n; i++) {
+      // v.emplace_back(nums[i], i);
+      ma[nums[i]].emplace(i);
+    }
+    // sort(v.begin(), v.end());
+
+    return ans;
+  }
+};
