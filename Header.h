2db9cf84985aa21ad40a269e7032152112edd7c3
zzzode <antifragile_zju@126.com>
2021-01-22 16:45:56 +0800

    add sols

---
+++ Header.h
@@ -0,0 +1,59 @@
+//
+// Created by zode on 2020/9/13.
+//
+
+#ifndef LEETCODE_HEADER_H
+#define LEETCODE_HEADER_H
+
+#include <algorithm>
+#include <cmath>
+#include <functional>
+//#include <hash_map>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+// using pii = pair<int, int>;
+// Definition for a binary tree node.
+struct TreeNode {
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  explicit TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+struct ListNode {
+  int val;
+  ListNode* next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+class Node {
+public:
+  int val;
+  Node* left;
+  Node* right;
+  Node* next;
+
+  Node() : val(0), left(nullptr), right(nullptr), next(nullptr) {}
+
+  explicit Node(int _val)
+      : val(_val), left(nullptr), right(nullptr), next(nullptr) {}
+
+  Node(int _val, Node* _left, Node* _right, Node* _next)
+      : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+#endif  // LEETCODE_HEADER_H
